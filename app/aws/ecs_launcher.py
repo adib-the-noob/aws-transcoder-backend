d15c7e1c23d64384f1f34133b851c33216051b37
Adib Bin Billal <mdadib550@gmail.com>
2024-09-08 16:27:05 +0600

    code refractored

--- app/aws/ecs_launcher.py
+++
@@ -1,8 +0,0 @@
-# make a ecs_task laucher for lauching a container 
-
-def lauch_ecs_container():
-    """
-        - It will lauch container and save the public ip address on DB.
-        - send a req on particular server url. 
-    """
-    pass
--- app/aws/sqs_final_bucket_consumer.py
+++
@@ -1,7 +0,0 @@
-def listend_final_bucket_queue():
-    pass
-"""
-    listen final bucket queue. when all transcoding and uploading is done, an event will automatically created
-    Then all the information from that event will be extracted.
-    and will save to db.
-"""
